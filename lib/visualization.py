838b5bdf77a7a994a14516c2b055147fa7394f05
Miguel <m.h.ruiz9@gmail.com>
2022-10-27 22:04:30 +0200

    Pruebas de representación gráfica de los distritos en mapa
    Former-commit-id: 888235995ba23dcf2e418854607058dfd1df6361

--- lib/visualization.py
+++ lib/visualization.py
@@ -76,3 +76,114 @@ def rand_cmap(nlabels, type='bright', first_color_black=True, last_color_black=F
     return random_colormap
 
 
+def cmapcolor_to_hexcolor(cmap_color):
+
+    # https://gis.stackexchange.com/questions/394219/folium-draw-polygons-with-distinct-colours
+    color = np.round(np.array(cmap_color[:-1]) * 255).astype(int)
+    color = [str(hex(j))[2:] for j in color]
+    color = [c if len(c) == 2 else '0'+c for c in color]
+    color = '#' + ''.join(color).upper()
+    return color
+
+
+def add_categorical_legend(folium_map, title, colors, labels):
+  
+    # https://stackoverflow.com/questions/65042654/how-to-add-categorical-legend-to-python-folium-map
+    if len(colors) != len(labels):
+        raise ValueError("colors and labels must have the same length.")
+
+    color_by_label = dict(zip(labels, colors))
+    
+    legend_categories = ""     
+    for label, color in color_by_label.items():
+        legend_categories += f"<li><span style='background:{color}'></span>{label}</li>"
+        
+    legend_html = f"""
+    <div id='maplegend' class='maplegend'>
+      <div class='legend-title'>{title}</div>
+      <div class='legend-scale'>
+        <ul class='legend-labels'>
+        {legend_categories}
+        </ul>
+      </div>
+    </div>
+    """
+    script = f"""
+        <script type="text/javascript">
+        var oneTimeExecution = (function() {{
+                    var executed = false;
+                    return function() {{
+                        if (!executed) {{
+                             var checkExist = setInterval(function() {{
+                                       if ((document.getElementsByClassName('leaflet-top leaflet-right').length) || (!executed)) {{
+                                          document.getElementsByClassName('leaflet-top leaflet-right')[0].style.display = "flex"
+                                          document.getElementsByClassName('leaflet-top leaflet-right')[0].style.flexDirection = "column"
+                                          document.getElementsByClassName('leaflet-top leaflet-right')[0].innerHTML += `{legend_html}`;
+                                          clearInterval(checkExist);
+                                          executed = true;
+                                       }}
+                                    }}, 100);
+                        }}
+                    }};
+                }})();
+        oneTimeExecution()
+        </script>
+      """
+   
+
+    css = """
+
+    <style type='text/css'>
+      .maplegend {
+        z-index:9999;
+        float:right;
+        background-color: rgba(255, 255, 255, 1);
+        border-radius: 5px;
+        border: 2px solid #bbb;
+        padding: 10px;
+        font-size:12px;
+        positon: relative;
+      }
+      .maplegend .legend-title {
+        text-align: left;
+        margin-bottom: 5px;
+        font-weight: bold;
+        font-size: 90%;
+        }
+      .maplegend .legend-scale ul {
+        margin: 0;
+        margin-bottom: 5px;
+        padding: 0;
+        float: left;
+        list-style: none;
+        }
+      .maplegend .legend-scale ul li {
+        font-size: 80%;
+        list-style: none;
+        margin-left: 0;
+        line-height: 18px;
+        margin-bottom: 2px;
+        }
+      .maplegend ul.legend-labels li span {
+        display: block;
+        float: left;
+        height: 16px;
+        width: 30px;
+        margin-right: 5px;
+        margin-left: 0;
+        border: 0px solid #ccc;
+        }
+      .maplegend .legend-source {
+        font-size: 80%;
+        color: #777;
+        clear: both;
+        }
+      .maplegend a {
+        color: #777;
+        }
+    </style>
+    """
+
+    folium_map.get_root().header.add_child(folium.Element(script + css))
+
+    return folium_map
